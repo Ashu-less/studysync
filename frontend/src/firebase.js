43fc1532996d7205502412a8d625365047f6e6ac
Ashu-less <ashu.brant@gmail.com>
2025-06-12 05:14:28 -0500

    ui  cleaner and ready for pr

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBFOEze0apxIauIpE4xF3iiMmWWh1Ll0dw",
   authDomain: "studysync-2c489.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-6494FMN0M4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
